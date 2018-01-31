359497f5e4959e4ce7b186a90c9f180b79a3e24f
Jesus Bossa <jesusbossa@protonmail.com>
2018-01-30 20:45:40 -0500

    Se mejora el funcionamiento interno

--- gulpfile.js
+++ gulpfile.js
@@ -10,4 +10,9 @@ gulp.task('styles', () => {
     .pipe(gulp.dest('public'))
 })
 
-gulp.task('default', ['styles'])
+gulp.task('assets', () => {
+    gulp
+        .src('assets/*')
+        .pipe(gulp.dest('public'))
+})
+gulp.task('default', ['styles', 'assets'])
--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 let express = require('express')
 let app = express()
 
+//Se le dice a express que escoga pug como motor de vistas
 app.set('view engine', 'pug')
 
+//Middleware que le dice a express que los archivos que se van a utilizar estan en la carpeta public
+app.use(express.static('public'))
+
+//Get de pagina home
 app.get('/', (req, res) => {res.render('index')})
 
+//Montando el servidor en el puerto 3000
 app.listen(3000, (err) => {
     if (err) return console.log('Hubo un error'), process.exit(1)
 
