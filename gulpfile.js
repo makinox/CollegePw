08875d89eb6009ce98b48415f4c2b46c40ed9b5d
Jesus Bossa <jesusbossa@protonmail.com>
2018-01-31 18:41:41 -0500

    Se automatiza el bundling

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,14 @@ const rename = require('gulp-rename')
 const babel = require('babelify')
 const browserify = require('browserify')
 const source = require('vinyl-source-stream')
+const watchify = require('watchify')
 
 gulp.task('styles', () => {
     gulp
-    .src('index.scss')
-    .pipe(sass())
-    .pipe(rename('app.css'))
-    .pipe(gulp.dest('public'))
+        .src('index.scss')
+        .pipe(sass())
+        .pipe(rename('app.css'))
+        .pipe(gulp.dest('public'))
 })
 
 gulp.task('assets', () => {
@@ -19,13 +20,31 @@ gulp.task('assets', () => {
         .pipe(gulp.dest('public'))
 })
 
-gulp.task('scripts', () => {
-    browserify('./lib/index.js')
-        .transform(babel)
-        .bundle()
-        .pipe(source('index.js'))
-        .pipe(rename('app.js'))
-        .pipe(gulp.dest('public'))
-})
+function compile(watch) {
+    let bundle = watchify(browserify('./lib/index.js'))
 
-gulp.task('default', ['styles', 'assets', 'scripts'])
+    function rebundle() {
+        bundle
+            .transform(babel)
+            .bundle()
+            .pipe(source('index.js'))
+            .pipe(rename('app.js'))
+            .pipe(gulp.dest('public'))
+    }
+
+    if (watch) {
+        bundle.on('update', () => {
+            console.log('Bundling...')
+            rebundle();
+        })
+    }
+
+    rebundle()
+
+}
+
+gulp.task('build', () => {return compile()})
+
+gulp.task('watch', () => {return compile(true)})
+
+gulp.task('default', ['styles', 'assets', 'build'])
