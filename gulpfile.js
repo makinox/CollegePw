c1a6672100a99d78613d426a5423434d40bd28a9
Jesus Bossa <jesusbossa@protonmail.com>
2018-02-04 10:24:07 -0500

    Se termina la primera pagina de inicio

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const rename = require('gulp-rename')
 const babel = require('babelify')
 const browserify = require('browserify')
 const source = require('vinyl-source-stream')
-const watchify = require('watchify')
 
 gulp.task('styles', () => {
     gulp
@@ -20,31 +19,13 @@ gulp.task('assets', () => {
         .pipe(gulp.dest('public'))
 })
 
-function compile(watch) {
-    let bundle = watchify(browserify('./lib/index.js'))
-
-    function rebundle() {
-        bundle
-            .transform(babel)
-            .bundle()
-            .pipe(source('index.js'))
-            .pipe(rename('app.js'))
-            .pipe(gulp.dest('public'))
-    }
-
-    if (watch) {
-        bundle.on('update', () => {
-            console.log('Bundling...')
-            rebundle();
-        })
-    }
-
-    rebundle()
-
-}
-
-gulp.task('build', () => {return compile()})
-
-gulp.task('watch', () => {return compile(true)})
+gulp.task('build', () => {
+    browserify('./lib/index.js')
+        .transform(babel)
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public'))
+})
 
 gulp.task('default', ['styles', 'assets', 'build'])
