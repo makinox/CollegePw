fc7b1e4a9a93bfca9ae3617cf1c50f431e1ee0e3
Jesus Bossa <jesusbossa@protonmail.com>
2018-02-04 19:47:09 -0500

    Heroku listo y se empieza el modulo calendario

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-let express = require('express')
-let app = express()
+const express = require('express')
+const app = express()
+
+//Inicializando el puerto dinamico local y el de heroku
+app.set('port', (process.env.PORT || 5000))
 
 //Se le dice a express que escoga pug como motor de vistas
 app.set('view engine', 'pug')
@@ -16,9 +19,7 @@ app.get('/home', (req, res) => {res.render('index')})
 //get de la pagina de noticias
 app.get('/profile', (req, res) => {res.render('index')})
 
-//Montando el servidor en el puerto 3000
-app.listen(3000, (err) => {
-    if (err) return console.log('Hubo un error'), process.exit(1)
-
-    console.log('Utopia escuchando en el puerto 3000')
-})
+//Montando el servidor en el puerto 5000
+app.listen(app.get('port'), function() {
+    console.log('Utopia corriendo en el puerto ', app.get('port'));
+  })
