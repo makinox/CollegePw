a111a0b89ee789ed815fc6ab88972a8962200429
Jesus bossa <jesusbossa@protonmail.com>
2018-10-03 14:01:06 -0500

    Home initialized

--- src/dist/core/routes.js
+++ src/dist/core/routes.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import Home from '../sections/containers/home'
+
 export default (props) => (
   <Router>
     <div>
       {props.children}
       <Switch>
-        {/* <Route exact path="/" component= /> */}
+        <Route exact path="/" component={Home} />
       </Switch>
     </div>
   </Router>
---
+++ src/dist/sections/containers/home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Home from '../components/homeLayout'
+
+export default class extends React.Component {
+  render() {
+    return <Home />
+  }
+}
