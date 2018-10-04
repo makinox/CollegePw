e1dd9b9c05d1b0528f5fca6b4ebddb024a1e657e
Jesus bossa <jesusbossa@protonmail.com>
2018-10-03 22:52:29 -0500

    Home improved

--- src/dist/sections/components/homeLayout.js
+++ src/dist/sections/components/homeLayout.js
@@ -11,7 +11,6 @@ export default () => (
       <div className="homeLayout-hero">
 
         <div className="homeLayout-hero-container">
-
           <div className="homeLayout-hero-head">
             <h1>Con <i>Utopía</i> todos podemos estudiar</h1>
             <p>Mira que te lo que te ofrece utopía para ti</p>
@@ -20,7 +19,6 @@ export default () => (
             <span className="btn">Saber más</span>
             <span className="btn">Iniciar sesión</span>
           </div>
-
         </div>
 
         <div>
@@ -31,7 +29,7 @@ export default () => (
 
       <div className="homeLayout-explain">
 
-        <div className="homeLayout-explain-card">
+        <div className="card">
           <h2>Qué es utopía</h2>
           <p>Utopia es una plataforma que brinda el servicio de gestión educacional a cualquier institución educativa de todos los niveles.
           <br />
@@ -39,7 +37,7 @@ export default () => (
         </p>
         </div>
 
-        <div className="homeLayout-explain-card">
+        <div className="card">
           <h2>Qué hacemos</h2>
           <p>Utopía te ofrece sus servicios de gestión a la comunidad completamente gratis.
           <br />
@@ -55,9 +53,9 @@ export default () => (
 
     <div className="homeLayout-footer">
       <div>
-        <iframe title="Utopia apps.co" width="560" height="315" src="https://www.youtube.com/embed/2le9qLtoqV4" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+        <iframe title="Utopia apps.co" width="100%" align="middle" height="315" src="https://www.youtube.com/embed/2le9qLtoqV4" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
       </div>
-      <div className="homeLayout-footer-card">
+      <div className="card homeLayout-footer-card">
         <h2>En utopía nos importas tu</h2>
         <p>Entra y descubre todo lo que tenemos para ti</p>
         <span className="btn">Comencemos!</span>
