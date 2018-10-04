e67bc9ff420aafee38a7303c6e6abb4a45abf993
Jesus bossa <jesusbossa@protonmail.com>
2018-10-03 21:44:42 -0500

    Images to home added

--- src/dist/sections/components/homeLayout.js
+++ src/dist/sections/components/homeLayout.js
@@ -1,52 +1,61 @@
 import React from 'react'
 
+import img1 from '../../../assets/home/img1.svg'
 import './homeLayout.css'
 
 export default () => (
   <section>
 
-    <div className="homeLayout-hero">
+    <div className="homeLayout-union">
 
-      <div className="homeLayout-hero-container">
-        <div className="homeLayout-hero-head">
-          <h1>Con <i>Utopía</i> todos podemos estudiar</h1>
-          <p>Mira que te lo que te ofrece utopía para ti</p>
+      <div className="homeLayout-hero">
+
+        <div className="homeLayout-hero-container">
+
+          <div className="homeLayout-hero-head">
+            <h1>Con <i>Utopía</i> todos podemos estudiar</h1>
+            <p>Mira que te lo que te ofrece utopía para ti</p>
+          </div>
+          <div className="homeLayout-hero-footer">
+            <span className="btn">Saber más</span>
+            <span className="btn">Iniciar sesión</span>
+          </div>
+
         </div>
-        <div className="homeLayout-hero-footer">
-          <span className="btn">Saber más</span>
-          <span className="btn">Iniciar sesión</span>
+
+        <div>
+          <img src={img1} alt="Utopia de logo de utopia decorado" />
         </div>
+
       </div>
 
-      <div></div>
+      <div className="homeLayout-explain">
 
-    </div>
-
-    <div className="homeLayout-explain">
-
-      <div className="homeLayout-explain-card">
-        <h2>Qué es utopía</h2>
-        <p>Utopia es una plataforma que brinda el servicio de gestión educacional a cualquier institución educativa de todos los niveles.
+        <div className="homeLayout-explain-card">
+          <h2>Qué es utopía</h2>
+          <p>Utopia es una plataforma que brinda el servicio de gestión educacional a cualquier institución educativa de todos los niveles.
           <br />
-          Nos enfocammos en aquellas entidades que cuentan con pocos recursos y poca disponibilidad tecnologica.
+            Nos enfocammos en aquellas entidades que cuentan con pocos recursos y poca disponibilidad tecnologica.
         </p>
-      </div>
+        </div>
 
-      <div className="homeLayout-explain-card">
-        <h2>Qué hacemos</h2>
-        <p>Utopía te ofrece sus servicios de gestión a la comunidad completamente gratis.
+        <div className="homeLayout-explain-card">
+          <h2>Qué hacemos</h2>
+          <p>Utopía te ofrece sus servicios de gestión a la comunidad completamente gratis.
           <br />
-          Te ayudamos a gestionar la información de estudiantes, profesores y tutores, asi mejorando la comunicación entre ellos.
+            Te ayudamos a gestionar la información de estudiantes, profesores y tutores, asi mejorando la comunicación entre ellos.
           <br />
-          Usamos algoritmos de inteligencia artificial para predecir e identificar falencias y mejoras en los estudiantes.
+            Usamos algoritmos de inteligencia artificial para predecir e identificar falencias y mejoras en los estudiantes.
         </p>
+        </div>
+
       </div>
 
     </div>
 
     <div className="homeLayout-footer">
       <div>
-      <iframe title="Utopia apps.co" width="560" height="315" src="https://www.youtube.com/embed/2le9qLtoqV4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+        <iframe title="Utopia apps.co" width="560" height="315" src="https://www.youtube.com/embed/2le9qLtoqV4" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
       </div>
       <div className="homeLayout-footer-card">
         <h2>En utopía nos importas tu</h2>
