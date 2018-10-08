99b8b969c63e1cbaa8e82095a9363aa5723b9799
Jesús Bossa <me@jesusbossa.com>
2018-10-08 12:39:02 -0500

    Login style improved

--- src/dist/sections/components/loginLayout.js
+++ src/dist/sections/components/loginLayout.js
@@ -1,5 +1,29 @@
 import React from 'react'
 
-export default () => (
- <h1>Hola</h1>
+import './loginLayout.css'
+
+export default (props) => (
+    <section className="section-container-login">
+    <div className="section-container-login-card">
+      <div className="section-container-login-card-header">
+        <h2>Utopía</h2>
+        <h3>Iniciar sesión</h3>
+      </div>
+      <div className="section-container-login-card-body">
+        <form onSubmit={props.submit} >
+          <div>
+            <label htmlFor="user" >Ingresa tu usuario</label>
+            <input type="text" name="user" />
+          </div>
+          <div>
+            <label htmlFor="password">Ingresa tu contraseña</label>
+            <input type="password" name="password" />
+          </div>
+          <div>
+            <button type="submit" className="btn">Ingresar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
 )
