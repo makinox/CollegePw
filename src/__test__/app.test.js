4f86e2afbf11839f13460f684b52057a7c49f738
Jesús Bossa <me@jesusbossa.com>
2018-10-30 10:48:06 -0500

    [Add] Dockerfiles added

---
+++ src/__test__/app.test.js
@@ -0,0 +1,21 @@
+import App from '../App'
+
+describe('Test app root works', () => {
+    
+  it('Project exits', () => {
+    expect(App).toBeTruthy()
+  })
+
+})
+
+describe('Project root is not null', () => {
+  it('App is not null', () => {
+    expect(App).not.toBeNull()
+  })
+})
+
+describe('Project is not undefined', () => {
+  it('App is not undefined', () => {
+    expect(App).not.toBeUndefined()
+  })
+})
