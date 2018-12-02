7e7a2f9ad11b6eb454b0f7841db1112e67b251b9
Jesús Bossa <me@jesusbossa.com>
2018-12-02 16:31:43 -0500

    [IMPROVEMENT] Nav blog

---
+++ src/dist/menu/menu.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './menu.css'
+
+export default () => (
+  <nav className="menu">
+    <ul>
+      <li>Home</li>
+      <li>Blog</li>
+      <li>Chat</li>
+    </ul>
+  </nav>
+)
